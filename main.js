70f403078d3d00a00d8656db3e1783e1de3432fd
FreeHi <freehi@cumallover.me>
2021-05-01 16:39:55 +0800

    Attempt 4

--- main.js
+++ main.js
@@ -1444,7 +1444,6 @@ function fullscreen(){
       document.fullScreenElement && null !== document.fullScreenElement || !document.mozFullScreen && !document.webkitIsFullScreen ? document.documentElement.requestFullScreen ? document.documentElement.requestFullScreen() : document.documentElement.mozRequestFullScreen ? document.documentElement.mozRequestFullScreen() : document.documentElement.webkitRequestFullScreen && document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT) : document.cancelFullScreen ? document.cancelFullScreen() : document.mozCancelFullScreen ? document.mozCancelFullScreen() : document.webkitCancelFullScreen && document.webkitCancelFullScreen();
 }
 
-let deferredPrompt;
 const addBtn = document.querySelector('#addtohomescreen');
 
 window.addEventListener('beforeinstallprompt', (e) => {
