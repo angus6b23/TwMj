cc7e7d49220f1561ed6eb1d8c8a3cdcb7df7973b
FreeHi <freehi@cumallover.me>
2021-05-08 16:28:36 +0800

    Attempt 5

--- main.js
+++ main.js
@@ -1445,7 +1445,7 @@ function fullscreen(){
 }
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('sw.js')
   .then((reg) => {
     // registration worked
     console.log('Registration succeeded.');
