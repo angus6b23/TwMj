ba0f4e37a6024af9aa54409938dc3407c27445c0
Angus Wan <work@12a.app>
2021-07-20 16:01:05 +0800

    Debugging 7

--- sw.js
+++ sw.js
@@ -16,28 +16,27 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('Handling fetch event for', event.request.url);
+    console.log('Handling fetch event for', event.request.url);
 
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        console.log('Found response in cache:', response);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request).then(function(response) {
+                if (response && response.status == 200 && response.type =='basic'){
+                    caches.open(webversion).then(function(cache){
+                        cache.put(event.request, response.clone());
+                        return response
+                    })
+                }
+            }).catch(function(error) {
+                console.error('Fetching failed:', error);
 
-        return response;
-      }
-      console.log('No response found in cache. About to fetch from network...');
-
-      return fetch(event.request).then(function(response) {
-        console.log('Response from network is:', response);
-
-        return response;
-      }).catch(function(error) {
-        console.error('Fetching failed:', error);
-
-        throw error;
-      });
-    })
-  );
+                throw error;
+            });
+        })
+    );
 });
 
 
